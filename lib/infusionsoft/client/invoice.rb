0b63c5f5cf46745052f7bdb20d9485dbb252c31a
Michael Voropaiev <m.voropaiev@gmail.com>
2019-07-24 01:02:41 +0300

    fix invoice_get_payments method

--- lib/infusionsoft/client/invoice.rb
+++ lib/infusionsoft/client/invoice.rb
@@ -194,7 +194,7 @@ module Infusionsoft
       # @param [Integer] invoice_id
       # @return [Array<Hash>] returns an array of payments
       def invoice_get_payments(invoice_id)
-        response = xmlrpc('Invoice.getPayments', invoice_id)
+        response = xmlrpc('InvoiceService.getPayments', invoice_id)
       end
 
       # Locates an existing card in the system for a contact, using the last 4 digits.
